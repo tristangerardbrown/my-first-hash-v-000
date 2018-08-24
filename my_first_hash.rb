91e9160060daa0695fdc856a172cf7addb1170f4
Tristan Brown <tgbrown@stanford.edu>
2018-08-24 23:51:16 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,9 +31,9 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets" => 2]
+  shipping_manifest["muskets"] = 2
 
-  shipping_manifest["gun powder" => 4]
+  shipping_manifest["gun powder"] = 4
 
   shipping_manifest
 
